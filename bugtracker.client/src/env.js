6463ee258b337bef7a34c288704485de507dae7a
Payton Macdonald <paytonmacdonald49@gmail.com>
2021-04-30 13:09:10 -0600

    first push

---
+++ bugtracker.client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'payton49.us.auth0.com'
+export const audience = 'https://bugtracker'
+export const clientId = 'ghOSgLopYtwW4LcTY2yCEG6ThYzJADQB'
